26334f4291dd84e6b4c0afee11dccb609512d227
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-08 05:35:07 +0900

    #1.2 Static Pre Rendering

--- pages/about-us.js
+++ pages/about-us.js
@@ -1,7 +1,14 @@
+import {useState} from "react";
+
 export default function About() {
-    return (
-        <div>
-            <h1>Hello</h1>
-        </div>
-    );
+    const [counter, setCounter] = useState(0);
+    return (<div>
+        <h1>Counter {counter}</h1>
+        <button
+            onClick={() => {
+                setCounter((prev) => prev + 1);
+            }
+            }>
+                +</button>
+        </div>);
 }
