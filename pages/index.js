4a93d475e076802cd7ae82e385f3e952061b7b3d
Kim Tae Hyun <tjdrkr2580@naver.com>
2022-08-09 01:17:09 +0900

    #2.3 SSR

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,14 @@ import Seo from './Seo';
 import { useEffect, useState } from 'react';
 
 
-export default function Home() {
-    const [movies,setMovies] = useState([]);
-    useEffect(()=> {
-        (async() => {
-            const {results} = await 
-                (await fetch("/api/movies"
-            )
-        ).json();
-            setMovies(results);
-        })();
-    },[]);
-    
+export default function Home({results}) {  
+    console.log(results);
     return (
         <div>
         <div className="container">
             <Seo title="Home"/>
-            {!movies && <h4>Loading...</h4>}{
-                movies.map((movie) => (
+            {
+                results?.map((movie) => (
                 <div className="movie" key={movie.id}>
                 <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
                     <h4>{movie.original_title}</h4>
@@ -33,6 +23,7 @@ export default function Home() {
           gap: 20px;
         }
         .movie img {
+          cursor: pointer;
           max-width: 100%;
           border-radius: 12px;
           transition: transform 0.2s ease-in-out;
@@ -50,4 +41,14 @@ export default function Home() {
         </div>
     </div>
     );
+}
+
+export async function getServerSideProps() {
+    const {results} = await 
+    (await fetch("http://localhost:3000/api/movies")).json();
+    return {
+        props: {
+            results,
+        }
+    }
 }
